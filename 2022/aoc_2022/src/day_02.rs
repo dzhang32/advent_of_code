2ad177dce0bac4ea02ee67d1d154f5503393a1bc
David Zhang <32676710+dzhang32@users.noreply.github.com>
2024-04-01 20:33:56 +0100

    2022: solve day 2 part 2

--- 2022/aoc_2022/src/day_02.rs
+++ 2022/aoc_2022/src/day_02.rs
@@ -7,7 +7,7 @@ use crate::Part;
 pub fn solve(input_file_path: &Path, part: Part) -> () {
     match part {
         Part::Part1 => solve_part_1(input_file_path),
-        Part::Part2 => panic!("Solution to part 2 is not implemented yet."),
+        Part::Part2 => solve_part_2(input_file_path),
     };
 }
 
@@ -15,6 +15,12 @@ enum RPS {
     Rock,
     Paper,
     Scissors,
+}
+
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
 }
 
 fn solve_part_1(input_file_path: &Path) -> i32 {
@@ -74,6 +80,67 @@ fn solve_part_1(input_file_path: &Path) -> i32 {
     total_points
 }
 
+fn solve_part_2(input_file_path: &Path) -> i32 {
+    let rps_encoding = HashMap::from([("A", RPS::Rock), ("B", RPS::Paper), ("C", RPS::Scissors)]);
+
+    let outcome_encoding = HashMap::from([
+        ("X", Outcome::Lose),
+        ("Y", Outcome::Draw),
+        ("Z", Outcome::Win),
+    ]);
+
+    let games: Vec<Vec<String>> = read_to_string(input_file_path)
+        .unwrap()
+        .lines()
+        .map(|line| line.split_whitespace().map(|s| s.to_string()).collect())
+        .collect();
+
+    let mut total_points: i32 = 0;
+
+    for game in games {
+        let opponent_choice = rps_encoding.get(&game[0].as_str()).unwrap();
+        let outcome = outcome_encoding.get(&game[1].as_str()).unwrap();
+
+        let my_choice: RPS = match opponent_choice {
+            RPS::Rock => match outcome {
+                Outcome::Win => RPS::Paper,
+                Outcome::Draw => RPS::Rock,
+                Outcome::Lose => RPS::Scissors,
+            },
+            RPS::Paper => match outcome {
+                Outcome::Win => RPS::Scissors,
+                Outcome::Draw => RPS::Paper,
+                Outcome::Lose => RPS::Rock,
+            },
+            RPS::Scissors => match outcome {
+                Outcome::Win => RPS::Rock,
+                Outcome::Draw => RPS::Scissors,
+                Outcome::Lose => RPS::Paper,
+            },
+        };
+
+        let choice_points: i32 = match my_choice {
+            RPS::Rock => 1,
+            RPS::Paper => 2,
+            RPS::Scissors => 3,
+        };
+
+        let outcome_points: i32 = match outcome {
+            Outcome::Win => 6,
+            Outcome::Draw => 3,
+            Outcome::Lose => 0,
+        };
+
+        println!("{}", choice_points + outcome_points);
+
+        total_points += choice_points;
+        total_points += outcome_points;
+    }
+
+    println!("{}", total_points);
+    total_points
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,5 +150,11 @@ mod tests {
     fn test_solve_part_1() {
         let input_file_path = tests::data_path("day_02_part_1.txt");
         assert_eq!(solve_part_1(input_file_path.as_path()), 15);
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        let input_file_path = tests::data_path("day_02_part_1.txt");
+        assert_eq!(solve_part_2(input_file_path.as_path()), 12);
     }
 }
