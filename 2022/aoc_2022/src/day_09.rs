70be07e0bbca64d34de6c7d253f929e0b63c5863
David Zhang <32676710+dzhang32@users.noreply.github.com>
2024-04-20 21:37:20 +0100

    2022: solve day 9 part 2

--- 2022/aoc_2022/src/day_09.rs
+++ 2022/aoc_2022/src/day_09.rs
@@ -1,5 +1,5 @@
 use std::collections::HashSet;
-use std::fs;
+use std::fs::{self, read_to_string};
 use std::path::Path;
 
 use crate::Part;
@@ -13,14 +13,15 @@ struct Coord {
     y: i32,
 }
 
-struct RopeEnd {
+#[derive(Debug)]
+struct RopeKnot {
     coord: Coord,
     prev_coord: Coord,
 }
 
-impl RopeEnd {
-    fn new() -> RopeEnd {
-        RopeEnd {
+impl RopeKnot {
+    fn new() -> RopeKnot {
+        RopeKnot {
             coord: Coord { x: 0, y: 0 },
             prev_coord: Coord { x: 0, y: 0 },
         }
@@ -36,14 +37,16 @@ impl RopeEnd {
             _ => {}
         }
     }
-    fn adjacent(&self, tail: &RopeEnd) -> bool {
-        if (self.coord.x - tail.coord.x).abs() > 1 || (self.coord.y - tail.coord.y).abs() > 1 {
+    fn adjacent(&self, next_rope_knot: &RopeKnot) -> bool {
+        if (self.coord.x - next_rope_knot.coord.x).abs() > 1
+            || (self.coord.y - next_rope_knot.coord.y).abs() > 1
+        {
             return false;
         } else {
             return true;
         }
     }
-    fn drag(&self, tail: &mut RopeEnd) -> () {
+    fn drag(&self, tail: &mut RopeKnot) -> () {
         if !self.adjacent(tail) {
             tail.coord.x = self.prev_coord.x;
             tail.coord.y = self.prev_coord.y;
@@ -54,7 +57,7 @@ impl RopeEnd {
 pub fn solve(input_file_path: &Path, part: Part) -> () {
     match part {
         Part::Part1 => solve_part_1(input_file_path),
-        Part::Part2 => panic!("Solution to part 2 is not implemented yet."),
+        Part::Part2 => solve_part_2(input_file_path),
     };
 }
 
@@ -65,8 +68,8 @@ fn solve_part_1(input_file_path: &Path) -> u32 {
         .map(|l| l.split_whitespace().collect::<Vec<&str>>())
         .collect::<Vec<Vec<&str>>>();
 
-    let mut head = RopeEnd::new();
-    let mut tail = RopeEnd::new();
+    let mut head = RopeKnot::new();
+    let mut tail = RopeKnot::new();
     let mut visited = HashSet::new();
 
     for instruction in input {
@@ -84,14 +87,89 @@ fn solve_part_1(input_file_path: &Path) -> u32 {
     visited.len() as u32
 }
 
+fn solve_part_2(input_file_path: &Path) -> u32 {
+    let binding = read_to_string(input_file_path).unwrap();
+    let input = binding
+        .lines()
+        .map(|l| l.split_whitespace().collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+
+    let mut rope = Vec::with_capacity(10);
+    for _ in 0..10 {
+        rope.push(RopeKnot::new());
+    }
+    let mut visited = HashSet::new();
+
+    for instruction in input {
+        let direction = instruction[0];
+        let steps: usize = instruction[1].parse().unwrap();
+
+        for _ in 0..steps {
+            // First, move the head.
+            rope[0].step(direction);
+
+            // Then, drag the remaining knots.
+            for i in 0..9 {
+                if !rope[i].adjacent(&rope[i + 1]) {
+                    let diff = (
+                        rope[i].coord.x - rope[i + 1].coord.x,
+                        rope[i].coord.y - rope[i + 1].coord.y,
+                    );
+                    let (dx, dy) = match diff {
+                        // need to move up/left/down/right
+                        (0, 2) => (0, 1),
+                        (0, -2) => (0, -1),
+                        (2, 0) => (1, 0),
+                        (-2, 0) => (-1, 0),
+                        // need to move to the right diagonally
+                        (2, 1) => (1, 1),
+                        (2, -1) => (1, -1),
+                        // need to move to the left diagonally
+                        (-2, 1) => (-1, 1),
+                        (-2, -1) => (-1, -1),
+                        // need to move up/down diagonally
+                        (1, 2) => (1, 1),
+                        (-1, 2) => (-1, 1),
+                        (1, -2) => (1, -1),
+                        (-1, -2) => (-1, -1),
+                        // 🆕 need to move diagonally
+                        (-2, -2) => (-1, -1),
+                        (-2, 2) => (-1, 1),
+                        (2, -2) => (1, -1),
+                        (2, 2) => (1, 1),
+                        _ => panic!(""),
+                    };
+                    rope[i + 1].coord.x += dx;
+                    rope[i + 1].coord.y += dy;
+                }
+                if i == 8 {
+                    visited.insert(rope[i + 1].coord.clone());
+                }
+            }
+        }
+    }
+
+    println!("{}", visited.len());
+    visited.len() as u32
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::common::tests;
+    use rstest::rstest;
 
     #[test]
     fn test_solve_part_1() {
         let input_file_path = tests::data_path("day_09_part_1.txt");
         assert_eq!(solve_part_1(input_file_path.as_path()), 13);
+    }
+
+    #[rstest]
+    #[case("day_09_part_1.txt", 1)]
+    #[case("day_09_part_2.txt", 36)]
+    fn test_solve_part_2(#[case] input_path: &str, #[case] expected: u32) {
+        let input_file_path = tests::data_path(input_path);
+        assert_eq!(solve_part_2(input_file_path.as_path()), expected);
     }
 }
